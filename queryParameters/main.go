9ea0019ac52934f802122a057451825ee8671827
Rogério Limas Moreira <rogerio.moreira@coxautoinc.com>
2022-01-07 15:34:30 -0300

    Added queryParameters

---
+++ queryParameters/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+
+func QueryHandler(w http.ResponseWriter, r *http.Request) {
+	queryParams := r.URL.Query()
+	w.WriteHeader(http.StatusOK)
+
+	fmt.Fprintf(w, queryParams["id"][0])
+}
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/blah", QueryHandler)
+
+	srv := &http.Server{
+		Handler: r,
+		Addr: ":8080",
+		ReadTimeout: 10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
+}
